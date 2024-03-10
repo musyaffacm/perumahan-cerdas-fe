e994f790e9c92794dcb251cf8014c32cbc271715
musyaffacm <musyaffa220@gmail.com>
2024-03-10 23:42:34 +0700

    slicing-ui: homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -8,7 +6,11 @@ import Home from "./pages/Home";
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path="/">
+          <Route path="" element={<Home />} />
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,63 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="flex gap-x-5">
+      {MENU.map((item, index) => (
+        <Card
+          sx={{ maxWidth: 345 }}
+          key={index}
+          onClick={() => navigate(item.url)}
+        >
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="40"
+              image={item.image}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {item.label}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {item.desc}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+const MENU = [
+  {
+    label: "Penghuni",
+    image: "/images/cards/family.jpg",
+    url: "/resident",
+    desc: "Kelola data penghuni perumahan, tambah atau ubah informasi dengan mudah.",
+  },
+  {
+    label: "Rumah",
+    image: "/images/cards/house.jpg",
+    url: "/house",
+    desc: "Pantaui status rumah dan catatan historis penghuninya, perbaharui dengan cepat.",
+  },
+  {
+    label: "Pembayaran",
+    image: "/images/cards/payment.jpg",
+    url: "/payment",
+    desc: "Rekam pembayaran iuran bulanan, akses laporan ringkasan dan detail dengan cepat.",
+  },
+];
+
+export default Home;
