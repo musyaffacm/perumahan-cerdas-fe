6d7851fe8d0534df5a3deadc00b82080aec51324
musyaffacm <musyaffa220@gmail.com>
2024-03-11 20:15:26 +0700

    create custom modal component

---
+++ src/composnents/CustomModal.jsx
@@ -0,0 +1,31 @@
+import { Box, Modal } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 800,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const CustomModal = (props) => {
+  const { open = false, onClose = () => {}, children } = props;
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style} className="flex flex-col gap-5">
+        {children}
+      </Box>
+    </Modal>
+  );
+};
+
+export default CustomModal;
